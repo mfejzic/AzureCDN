8246b9ab2f11179707250a8be143e9d9217810a0
Muhamed Fejzic <91850725+mfejzic@users.noreply.github.com>
2024-12-27 17:02:07 -0600

    Update providers.tf

--- AzureCDN/providers.tf
+++ AzureCDN/providers.tf
@@ -28,10 +28,10 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "064b2bea-3fc1-4bf7-b067-3d942c23e4dd"    // find this in the subscription overview
-  tenant_id = "16983dae-9f48-4a35-b9f5-0519bf3cdf09"          // find this in the entra overview
-  client_id = "3728e04a-d9d3-4d3c-b503-b287b1aaa666"          // find in entra -> manage -> app registrations -> all applications -> use terraform client ID
-  client_secret = "KSB8Q~VJGcVoz-k8nATIMhZJuBxXRU2s-xfFTc5P"      // find in entra -> manage -> app registrations -> all applications -> terraform -> manage -> certificates & secrets -> client secrets
+  subscription_id = ""    // find this in the subscription overview
+  tenant_id = ""          // find this in the entra overview
+  client_id = ""          // find in entra -> manage -> app registrations -> all applications -> use terraform client ID
+  client_secret = ""      // find in entra -> manage -> app registrations -> all applications -> terraform -> manage -> certificates & secrets -> client secrets
 }
 
 
